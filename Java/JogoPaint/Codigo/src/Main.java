7baa9ba3e5475cd3057ef911b3af95759f8c6fb1
Hilster Barbosa Santos <69377592+Hilster00@users.noreply.github.com>
2022-12-15 02:23:00 -0300

    Add files via upload

---
+++ Java/JogoPaint/Codigo/src/Main.java
@@ -0,0 +1,18 @@
+package src;
+
+import java.awt.EventQueue;
+
+public class Main {
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Janela frame = new Janela();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+}
